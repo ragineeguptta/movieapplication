c620cb0652fc3cb5f6b11b6539b7eca21861a932
Raginee <raginee.gupta@hirdhav.com>
2022-07-16 15:00:55 +0530

    2

--- Model/Movie.cs
+++ Model/Movie.cs
@@ -8,5 +8,8 @@ namespace movieapplication.Model
         public int MovieId { get; set; }
         public string MovieName { get; set; }
         public string ProducerName { get; set; }
+        public DateTime DateofRelease { get; set; }
+
+        public ICollection<Actor> Actors { get; set; }
     }
 }
--- Model/MovieActorId.cs
+++ Model/MovieActorId.cs
@@ -5,12 +5,10 @@ namespace movieapplication.Model
 {
     public class MovieActorId
     {
-        [Key]
-        public int Id { get; set; }
-
         public int MovieId { get; set; }
         public int ActorId { get; set; }
 
-
+        public Movie Movie { get; set; }
+        public Actor Actor { get; set; }
     }
 }
